1535f7bd9768198d587483e79dd1eaa4161a18bb
Roman Strobl <roman.strobl@wultra.com>
2019-09-18 13:09:12 +0200

    Fix #675: Filter IPv6 IP addresses when detecting client IP address

--- powerauth-webflow-authentication/src/main/java/io/getlime/security/powerauth/lib/webflow/authentication/interceptor/WebSocketHandshakeInterceptor.java
+++ powerauth-webflow-authentication/src/main/java/io/getlime/security/powerauth/lib/webflow/authentication/interceptor/WebSocketHandshakeInterceptor.java
@@ -17,6 +17,12 @@ import java.util.Map;
  */
 public class WebSocketHandshakeInterceptor implements HandshakeInterceptor {
 
+    private final boolean forceIpv4;
+
+    public WebSocketHandshakeInterceptor(boolean forceIpv4) {
+        this.forceIpv4 = forceIpv4;
+    }
+
     @Override
     public boolean beforeHandshake(@NonNull ServerHttpRequest request, @NonNull ServerHttpResponse response,
                                    @NonNull WebSocketHandler wsHandler, @NonNull Map<String, Object> attributes) {
@@ -25,9 +31,20 @@ public class WebSocketHandshakeInterceptor implements HandshakeInterceptor {
         if (request instanceof ServletServerHttpRequest) {
             ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
             String ipAddress = servletRequest.getServletRequest().getHeader("X-FORWARDED-FOR");
-            if (ipAddress == null) {
+            if (forceIpv4) {
+                // IPv4 logic
+                if (ipAddress == null || !isIpv4Address(ipAddress)) {
+                    ipAddress = servletRequest.getServletRequest().getRemoteAddr();
+                    if (!isIpv4Address(ipAddress)) {
+                        // Fallback to 127.0.0.1 in case IPv4 address could not be determined
+                        ipAddress = "127.0.0.1";
+                    }
+                }
+            } else if (ipAddress == null) {
+                // IPv4 or IPv6 logic
                 ipAddress = servletRequest.getServletRequest().getRemoteAddr();
             }
+
             attributes.put("client_ip", ipAddress);
         }
         return true;
@@ -36,5 +53,16 @@ public class WebSocketHandshakeInterceptor implements HandshakeInterceptor {
     @Override
     public void afterHandshake(@NonNull ServerHttpRequest request, @NonNull ServerHttpResponse response,
                                @NonNull WebSocketHandler wsHandler, Exception exception) {
+    }
+
+    /**
+     * Determine whether IP address is an IPv4 address.
+     * @param address IP address as String.
+     * @return Whether IP address is an IPv4 address.
+     */
+    private boolean isIpv4Address(String address) {
+        // Source: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch08s16.html
+        return address.matches("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}↵\n" +
+                "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
     }
 }
