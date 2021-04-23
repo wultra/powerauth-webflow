c64bc56b9bf107e4a3e6b39776e17368e5ec1cbd
Roman Strobl <roman.strobl@wultra.com>
2021-04-23 10:03:58 +0200

    Fix #987: Use null value in NO_USERNAME algorithm

--- powerauth-nextstep/src/main/java/io/getlime/security/powerauth/app/nextstep/service/CredentialGenerationService.java
+++ powerauth-nextstep/src/main/java/io/getlime/security/powerauth/app/nextstep/service/CredentialGenerationService.java
@@ -92,7 +92,7 @@ public class CredentialGenerationService {
                 }
 
             case NO_USERNAME:
-                return "";
+                return null;
 
             default:
                 throw new InvalidConfigurationException("Unsupported username generation algorithm: " + credentialPolicy.getUsernameGenAlgorithm());
