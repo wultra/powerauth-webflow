2c1c18c85b085f7d3521991c29f288bb74970fd9
Roman Strobl <roman.strobl@wultra.com>
2021-12-10 14:51:29 +0100

    Update copyrights

--- powerauth-webflow-authentication/src/main/java/io/getlime/security/powerauth/lib/webflow/authentication/service/WebSocketMessageService.java
+++ powerauth-webflow-authentication/src/main/java/io/getlime/security/powerauth/lib/webflow/authentication/service/WebSocketMessageService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 201ý Wultra s.r.o.
+ * Copyright 2017 Wultra s.r.o.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
