59674aa7fda7fef5ec9343df337eab5149ee78f9
Roman Strobl <roman.strobl@wultra.com>
2024-08-06 15:01:07 +0200

    Add logging

--- powerauth-webflow/src/main/java/io/getlime/security/powerauth/app/webflow/configuration/WebMvcConfiguration.java
+++ powerauth-webflow/src/main/java/io/getlime/security/powerauth/app/webflow/configuration/WebMvcConfiguration.java
@@ -26,6 +26,7 @@ import io.getlime.security.powerauth.rest.api.spring.annotation.support.PowerAut
 import io.getlime.security.powerauth.rest.api.spring.annotation.support.PowerAuthEncryptionArgumentResolver;
 import io.getlime.security.powerauth.rest.api.spring.annotation.support.PowerAuthWebArgumentResolver;
 import io.getlime.security.powerauth.rest.api.spring.filter.PowerAuthRequestFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -53,6 +54,7 @@ import java.util.List;
  * @author Petr Dvorak, petr@wultra.com
  */
 @Configuration
+@Slf4j
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Autowired
@@ -143,6 +145,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public Resource languageSettingSource() throws MalformedURLException {
         Resource resource = resourceLoader.getResource(configuration.getResourcesLocation() + "lang.json");
         if (!resource.exists()) {
+            logger.info("The lang.json file was not found in {}, using default location", configuration.getResourcesLocation());
             resource = new UrlResource("classpath:/static/resources/lang.json");
         }
         return resource;
