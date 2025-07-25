7cf7bee7d175f3a2724ec6163fd1b149b3de6bf0
Zdeněk Černý <zdenek.cerny@wultra.com>
2025-07-25 16:59:11 +0200

    Fix #1855: Default source and target columns in ns_credential_storage have wrong default
    (cherry picked from commit bb21b56c993c5a5fc10dfcc205fd8ae12ec612df)

--- docs/sql/postgresql/migration_1.9.0_1.10.0.sql
+++ docs/sql/postgresql/migration_1.9.0_1.10.0.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS shedlock (name VARCHAR(64) NOT NULL, lock_until TIMES
 
 -- Changeset powerauth-nextstep/1.10.x/20250715-add-location-setting::1::Zdenek Cerny
 -- Add column source to ns_credential_storage
-ALTER TABLE ns_credential_storage ADD source VARCHAR(32) DEFAULT 'local';
+ALTER TABLE ns_credential_storage ADD source VARCHAR(32) DEFAULT 'LOCAL';
 
 -- Changeset powerauth-nextstep/1.10.x/20250715-add-location-setting::2::Zdenek Cerny
 -- Add column target to ns_credential_storage
-ALTER TABLE ns_credential_storage ADD target VARCHAR(32) DEFAULT 'local';
+ALTER TABLE ns_credential_storage ADD target VARCHAR(32) DEFAULT 'LOCAL';
