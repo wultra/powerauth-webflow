760d54acf71408b172886d6c1187dbd22c508b5c
Martin Korbel <martin@pd.lc>
2025-10-03 17:00:29 +0200

    Fix #1904: Load jdbc driver from folder instead of included war

--- powerauth-nextstep/docker/docker-entrypoint.sh
+++ powerauth-nextstep/docker/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-exec java -Dserver.port=8080 -Dspring.config.additional-location=/app/application.properties ${JAVA_OPTS:-} -jar "${APP_PATH}"
+exec java -Dserver.port=8080 -Dspring.config.additional-location=/app/application.properties ${JAVA_OPTS:-} -cp "${APP_PATH}:/app/extlib/*" org.springframework.boot.loader.launch.WarLauncher
